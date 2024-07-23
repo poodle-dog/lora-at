101ce37a9f381e77bc50f004fdcdf5f210f9b6a1
Nash Reilly <nash.reilly@twosixtech.com>
2024-07-23 10:26:27 -0400

    Initial commit of stub lora_module.py

---
+++ lora_module.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+
+########################################################################
+### Python class for interacting with LoRA module via serial AT cmds ###
+########################################################################
+
+import serial
+import time
+
+class LoRaModule:
+    def __init__(self, port, baudrate=115200, timeout=1):
+        self.ser = serial.Serial(port, baudrate, timeout=timeout)
+
+    def send_command(self, command):
+        """Sends an AT command and waits for a response."""
+        command_with_newline = command + "\r\n"  
+        self.ser.write(command_with_newline.encode())
+
+        response_lines = []
+        while True:
+            line = self.ser.readline().decode().strip()
+            if not line:  # Empty line indicates end of response
+                break
+            response_lines.append(line)
+
+        if response_lines[-1] == "+OK":
+            return response_lines[0]  
+        else:
+            raise Exception(f"Error in response: {response_lines}")
