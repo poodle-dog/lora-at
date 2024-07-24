4f576f31bfd1ebbb638c74f2979b73f13a577542
Nash Reilly <nash.reilly@twosixtech.com>
2024-07-24 10:24:39 -0400

    Added example file that resets the attached LoRA module

---
+++ examples/lora_reset.py
@@ -0,0 +1,11 @@
+import argparse
+import sys
+from lora_module.lora_module import LoRaModule
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='example loraModule reset command')
+    parser.add_argument('device', type=str, help='Name of serial device file (e.g. /dev/ttyUSB0')
+    args = parser.parse_args()
+
+    lora = LoRaModule(args.device)
+    response = lora.reset()
