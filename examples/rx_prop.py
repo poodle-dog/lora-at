25bc70cc720f231e20f83b4f609ff1c4ffd98a0e
Nash Reilly <nash.reilly@twosixtech.com>
2024-07-25 12:23:22 -0400

    Added basic example LoRA server to listen

---
+++ examples/rx_prop.py
@@ -0,0 +1,21 @@
+import argparse
+import sys
+from lora_module.lora_module import LoRaProp
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='simple rx server for Proprietary LoRA')
+    parser.add_argument(
+            'device', 
+            type=str, 
+            help='Name of serial device file (e.g. /dev/ttyUSB0)'
+    )
+    args = parser.parse_args()
+
+    lora = LoRaProp(args.device)
+    print("Starting LoRA Rx server")
+    while True:
+        response = lora.get_received_data()
+        if response:
+            print(response)
+
+
