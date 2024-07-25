06e00dded95697b81ad5f4e754a480fd6410b26c
Nash Reilly <nash.reilly@twosixtech.com>
2024-07-25 15:38:53 -0400

    Updated reset example

--- examples/lora_reset.py
+++ examples/lora_reset.py
@@ -3,8 +3,14 @@ import sys
 from lora_module.lora_module import LoRaModule
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='example loraModule reset command')
-    parser.add_argument('device', type=str, help='Name of serial device file (e.g. /dev/ttyUSB0')
+    parser = argparse.ArgumentParser(
+            description='example loraModule reset command'
+    )
+    parser.add_argument(
+            'device', 
+            type=str,
+            help='Name of serial device file (e.g. /dev/ttyUSB0'
+    )
     args = parser.parse_args()
 
     lora = LoRaModule(args.device)
