29026ddc0a1b64a74885925a1d4563a9329ed45a
Nash Reilly <nash.reilly@twosixtech.com>
2024-09-03 15:38:27 -0400

    Restructured directory to be installable

---
+++ lora_module/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='lora_module',
+    version='0.1.0',
+    description='A module for LoRa communication',
+    author='Nash Reilly',
+    author_email='nash.reilly@twosixtech.com',
+    packages=find_packages(),  
+    install_requires=[
+        'serial',
+        'pyserial',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
